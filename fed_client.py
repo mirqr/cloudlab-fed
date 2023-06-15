902fadb55d2ae622e18630cce65d73f58bbffa4f
mirqr <9435373+mirqr@users.noreply.github.com>
2023-06-15 16:29:03 +0200

    Create fed_client.py

---
+++ fed_client.py
@@ -0,0 +1,55 @@
+import os
+
+import flwr as fl
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+
+
+# Make TensorFlow log less verbose
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+# Load model and data (MobileNetV2, CIFAR-10)
+#model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+#model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+#(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+
+# Load the MNIST dataset
+(x_train, y_train), (x_test, y_test)) = mnist.load_data()
+
+# Preprocess the data
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Define the model architecture
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+
+
+# Define Flower client
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=1, batch_size=5)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": accuracy}
+
+
+# Start Flower client
+fl.client.start_numpy_client(server_address="172.31.88.9:8080", client=CifarClient())
