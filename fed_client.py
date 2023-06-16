12d84f52e244549265549ee72f51481f42b1bf46
mirqr <9435373+mirqr@users.noreply.github.com>
2023-06-16 10:52:21 +0200

    Update fed_client.py

--- fed_client.py
+++ fed_client.py
@@ -17,14 +17,14 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 # Load model and data (MobileNetV2, CIFAR-10)
+#(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 #model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
 #model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-#(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
 
 
 # Load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
 # Preprocess the data
 x_train = x_train / 255.0
 x_test = x_test / 255.0
@@ -35,7 +35,6 @@ model = Sequential([
     Dense(64, activation='relu'),
     Dense(10, activation='softmax')
 ])
-
 # Compile the model
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
@@ -57,5 +56,25 @@ class CifarClient(fl.client.NumPyClient):
         return loss, len(x_test), {"accuracy": accuracy}
 
 
-# Start Flower client
-fl.client.start_numpy_client(server_address="172.31.88.9:8080", client=CifarClient())
+def start_flower_client(ip_server_address, port = "8080"):
+    # Create the full server address
+    server_address = ip_server_address+":"+port
+    
+    # Start Flower client
+    fl.client.start_numpy_client(server_address=server_address, client=CifarClient())
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Flower Client")
+    parser.add_argument('--ip_address', type=str, default="0.0.0.0", help='IP address of the server.')
+    return parser.parse_args()
+
+def main():
+    # Parse command line arguments
+    args = parse_arguments()
+    
+    server_address = args.ip_address
+
+    start_flower_client(server_address)
+
+if __name__ == "__main__":
+    main()
