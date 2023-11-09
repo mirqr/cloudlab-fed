dc5b339b74bdbc4ee40ab9a8fde7ee076c792578
mirqr <nardi.mir@gmail.com>
2023-11-09 09:41:26 +0100

    extend fl skeleton

--- src/fed_client.py
+++ src/fed_client.py
@@ -1,5 +1,9 @@
 import os
+import sys
+
 import argparse
+
+import logging
 
 import flwr as fl
 import tensorflow as tf
@@ -8,30 +12,26 @@ from tensorflow.keras.datasets import mnist, fashion_mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense
 
-# loggers output all messages to stdout in addition to log file
-import logging
-import sys
+
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
 
 # Make TensorFlow log less verbose
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-# Load model and data (MobileNetV2, CIFAR-10)
-DATA_FRACTION = 0.1
-def get_random_subset(x, y, fraction):
-    indices = np.random.choice(len(x), int(len(x) * fraction), replace=False)
-    return x[indices], y[indices]
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data() # all the dataset
+
+
+
+
+
+
+
+#(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data() # all the dataset
 
 # normalize 
-x_train, x_test = x_train / 255.0, x_test / 255.0
+#x_train, x_test = x_train / 255.0, x_test / 255.0
 # x_train_client, y_train_client = x_train, y_train # use all the dataset
-x_train_client, y_train_client = get_random_subset(x_train, y_train, DATA_FRACTION) # take a fraction of the dataset
-
-model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
-model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+#x_train_client, y_train_client = get_random_subset(x_train, y_train, DATA_FRACTION) # take a fraction of the dataset
 
 
 
@@ -50,27 +50,65 @@ model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
 
 # Define Flower client
+# Define Flower client
 class CifarClient(fl.client.NumPyClient):
+    def __init__(self, model, x_train, y_train, x_test, y_test):
+        self.model = model
+        self.x_train, self.y_train = x_train, y_train
+        self.x_test, self.y_test = x_test, y_test
+    
+
+    def get_properties(self, config):
+        """Get properties of client."""
+        raise Exception("Not implemented")
+
     def get_parameters(self, config):
-        return model.get_weights()
+        """Get parameters of client."""
+        return self.model.get_weights()
+
 
     def fit(self, parameters, config):
-        model.set_weights(parameters)
-        model.fit(x_train_client, y_train_client, epochs=3, batch_size=5)  # use the client's data subset
-        return model.get_weights(), len(x_train_client), {}
+        self.model.set_weights(parameters)
+
+        batch_size: int = config["batch_size"]
+        epochs: int = config["local_epochs"]
+
+        history = self.model.fit(
+            self.x_train,
+            self.y_train,
+            batch_size,
+            epochs,
+            validation_split=0.0,
+        )
+
+        # Return updated model parameters and results
+        parameters_prime = self.model.get_weights()
+        num_examples_train = len(self.x_train)
+        results = {
+            "loss": history.history["loss"][0],
+            "accuracy": history.history["accuracy"][0],
+            #"val_loss": history.history["val_loss"][0],
+            #"val_accuracy": history.history["val_accuracy"][0],
+        }
+        return parameters_prime, num_examples_train, results
 
     def evaluate(self, parameters, config):
-        model.set_weights(parameters)
-        loss, accuracy = model.evaluate(x_test, y_test)
-        return loss, len(x_test), {"accuracy": accuracy}
+        """Evaluate parameters on the locally held test set."""
+
+        # Update local model with global parameters
+        self.model.set_weights(parameters)
+
+        # Get config values
+        steps: int = config["val_steps"]
+
+        # Evaluate global model parameters on the local test data and return results
+        loss, accuracy = self.model.evaluate(self.x_test, self.y_test, 32, steps=steps)
+        num_examples_test = len(self.x_test)
+        return loss, num_examples_test, {"accuracy": accuracy}
 
 
-def start_flower_client(ip_server_address, port = "8080"):
-    # Create the full server address
-    server_address = ip_server_address+":"+port
-    
-    # Start Flower client
-    fl.client.start_numpy_client(server_address=server_address, client=CifarClient())
+
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Flower Client")
@@ -83,9 +121,50 @@ def main():
     # Parse command line arguments
     args = parse_arguments()
     
-    server_address = args.ip_address
 
-    start_flower_client(server_address)
+    # model and data
+    model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+    model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+    
+    DATA_FRACTION=0.1
+    (x_train, y_train), (x_test, y_test) = load_partition(fraction=DATA_FRACTION)
+    n = 5
+    x_train, y_train = get_partition(x_train, y_train, i=0, num_partitions=n) # take a fraction of the dataset
+    print('x_train.shape: ', x_train.shape)
+
+    # Start Flower client
+    client = CifarClient(model, x_train, y_train, x_test, y_test)
+
+    server_ip = args.ip_address
+    port = "8080"
+    server_address = server_ip+":"+port
+    fl.client.start_numpy_client(
+        server_address=server_address, 
+        client=client)
+
+
+def get_random_subset(x, y, fraction=0.1):
+    indices = np.random.choice(len(x), int(len(x) * fraction), replace=False)
+    return x[indices], y[indices]
+
+def get_partition(x, y, i, num_partitions):
+    size = len(x) // num_partitions
+    start = i * size
+    end = start + size
+    return x[start:end], y[start:end]
+
+def load_partition(fraction=0.1):
+    # Load the MNIST dataset
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data(); x_train = x_train / 255.0; x_test = x_test / 255.0
+
+    # normalize 
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    #x_train_client, y_train_client = x_train, y_train # use all the dataset
+    x_train_client, y_train_client = get_random_subset(x_train, y_train, fraction=fraction) # take a fraction of the dataset
+
+    return (x_train_client, y_train_client), (x_test, y_test)
+
+
 
 if __name__ == "__main__":
     main()
--- src/fed_server.py
+++ src/fed_server.py
@@ -4,8 +4,73 @@ import argparse
 # loggers output all messages to stdout in addition to log file
 import logging
 import sys
+from datetime import datetime
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+def fit_config(server_round: int):
+    """Return training configuration dict for each round.
+
+    Keep batch size fixed at 32, perform two rounds of training with one
+    local epoch, increase to two local epochs afterwards.
+    """
+    config = {
+        "batch_size": 8,
+        "local_epochs": 2
+        #"local_epochs": 2 if server_round < 2 else 5
+    }
+    return config
+
+def evaluate_config(server_round: int):
+    """Return evaluation configuration dict for each round.
+
+    Perform five local evaluation steps on each client (i.e., use five batches) during
+    rounds one to three, then increase to ten local evaluation steps.
+    """
+    val_steps = 5 if server_round < 4 else 10
+    return {"val_steps": val_steps}
+
+def start_flower_server(ip_address, port = "8080", rounds = 3):
+    # Create the full server address
+    server_address = ip_address+":"+port
+    print("----> Server address: "+server_address, 'num_rounds: ', rounds)
+
+    strategy = fl.server.strategy.FedAvg(
+        # Fraction of clients used during training. In case min_fit_clients > fraction_fit * available_clients, min_fit_clients will still be sampled. Defaults to 1.0.
+        fraction_fit=1, # 0.1,  
+        #fraction_eval=0.1,
+        min_fit_clients=2,       # Minimum number of clients used during training. Default 2. Always >= min_available_clients.
+        min_available_clients=2, # Minimum number of total clients in the system. server will wait until at least 2 clients are connected.
+        #eval_fn=None,
+        on_fit_config_fn=fit_config,
+        on_evaluate_config_fn=evaluate_config,
+        #initial_parameters=None,
+    )
+
+    start_time = datetime.now()
+    with open('server_log.txt', 'a') as f:
+        f.write('\nserver started at: ' + str(start_time))
+
+    # Start Flower server
+    fl.server.start_server(
+        server_address=server_address,
+        strategy=strategy,
+        config=fl.server.ServerConfig(num_rounds=rounds),
+    )
+    
+    
+    # write a file to indicate that the server has finished. and the time (date and time it finished)
+    end_time = datetime.now()
+    with open('server_log.txt', 'a') as f:
+        f.write('\nserver finished at: ' + str(end_time))
+        f.write('\nserver took: ' + str(end_time - start_time))
+        f.write('\n----------------------------------')
+        f.write('\n\n')
+
+    # print server.log
+    with open('server_log.txt', 'r') as f:
+        print(f.read())
 
 # Define a function to parse command line arguments
 def parse_arguments():
@@ -14,28 +79,8 @@ def parse_arguments():
     parser.add_argument('--rounds', type=int, default=3, help='Number of rounds to train.')
     return parser.parse_args()
 
-def start_flower_server(ip_address, port = "8080", rounds = 3):
-    # Create the full server address
-    server_address = ip_address+":"+port
-    print("----> Server address: "+server_address, 'num_rounds: ', rounds)
 
-    strategy = fl.server.strategy.FedAvg(
-        fraction_fit=0.1,
-        #fraction_eval=0.1,
-        min_fit_clients=2,
-        min_available_clients=2,
-        #eval_fn=None,
-        #on_fit_config_fn=None,
-        #on_evaluate_config_fn=None,
-        #initial_parameters=None,
-    )
-
-    # Start Flower server
-    fl.server.start_server(
-        server_address=server_address,
-        strategy=strategy,
-        config=fl.server.ServerConfig(num_rounds=rounds),
-    )
+    
 
 def main():
     # Parse command line arguments
